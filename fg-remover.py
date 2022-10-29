ecb30c09f8bf77410cca8c8c3df7e60ea402687e
patrickslarson8 <pat.s.larson8@gmail.com>
2022-10-29 14:16:56 -0500

    Combine menu selection and preview w/ mediapipe

--- fg-remover.py
+++ fg-remover.py
@@ -3,8 +3,13 @@ import PySimpleGUI as sg
 from PIL import Image
 import cv2
 import numpy as np
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_selfie_segmentation = mp.solutions.selfie_segmentation
 
+BG_COLOR = (192, 192, 192)
 
+## Layout columns
 file_list_column = [
     [
         sg.Text("Image Folder"),
@@ -17,43 +22,6 @@ file_list_column = [
         )
     ],
 ]
-
-# # Run the Event Loop
-# while True:
-#     event, values = window.read()
-#     if event == "Exit" or event == sg.WIN_CLOSED:
-#         break
-#     # Folder name was filled in, make a list of files in the folder
-#     if event == "-FOLDER-":
-#         folder = values["-FOLDER-"]
-#         try:
-#             # Get list of files in folder
-#             file_list = os.listdir(folder)
-#         except:
-#             file_list = []
-
-#         fnames = [
-#             f
-#             for f in file_list
-#             if os.path.isfile(os.path.join(folder, f))
-#             and f.lower().endswith((".png", ".gif"))
-#         ]
-#         window["-FILE LIST-"].update(fnames)
-#     elif event == "-FILE LIST-":  # A file was chosen from the listbox
-#         try:
-#             filename = os.path.join(
-#                 values["-FOLDER-"], values["-FILE LIST-"][0]
-#             )
-#             window["-TOUT-"].update(filename)
-            
-#             with Image.Open(filename) as im:
-#                 im.show()
-            
-#             #window["-IMAGE-"].update(filename=filename)
-
-#         except:
-#             pass
-
 
 preview_column = [
     [sg.Text("Foreground Remover", size=(60, 1), justification="center")],
@@ -96,6 +64,48 @@ preview_column = [
     [sg.Button("Exit", size=(10, 1))],
 ]
 
+# cap = cv2.VideoCapture(0)
+
+# with mp_selfie_segmentation.SelfieSegmentation(
+#     model_selection=1) as selfie_segmentation:
+#   bg_image = None
+#   while cap.isOpened():
+#     success, image = cap.read()
+#     if not success:
+#       print("Ignoring empty camera frame.")
+#       # If loading a video, use 'break' instead of 'continue'.
+#       continue
+
+#     # Flip the image horizontally for a later selfie-view display, and convert
+#     # the BGR image to RGB.
+#     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+#     # To improve performance, optionally mark the image as not writeable to
+#     # pass by reference.
+#     image.flags.writeable = False
+#     results = selfie_segmentation.process(image)
+
+#     image.flags.writeable = True
+#     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+#     # Draw selfie segmentation on the background image.
+#     # To improve segmentation around boundaries, consider applying a joint
+#     # bilateral filter to "results.segmentation_mask" with "image".
+#     condition = np.stack(
+#       (results.segmentation_mask,) * 3, axis=-1) > 0.1
+#     # The background can be customized.
+#     #   a) Load an image (with the same width and height of the input image) to
+#     #      be the background, e.g., bg_image = cv2.imread('/path/to/image/file')
+#     #   b) Blur the input image by applying image filtering, e.g.,
+#     #      bg_image = cv2.GaussianBlur(image,(55,55),0)
+#     if bg_image is None:
+#       bg_image = np.zeros(image.shape, dtype=np.uint8)
+#       bg_image[:] = BG_COLOR
+#     output_image = np.where(condition, image, bg_image)
+
+#     cv2.imshow('MediaPipe Selfie Segmentation', output_image)
+#     if cv2.waitKey(5) & 0xFF == 27:
+#       break
+# cap.release()
 
 def main():
     sg.theme("LightGreen")
@@ -109,60 +119,96 @@ def main():
 
     # Create the window and show it without the plot
     window = sg.Window("Foreground Remover", layout, location=(800, 400))
-
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        event, values = window.read(timeout=20)
-        if event == "Exit" or event == sg.WIN_CLOSED:
-            break
-        
-        elif event == "-FOLDER-":
-            folder = values["-FOLDER-"]
-            try:
-                # Get list of files in folder
-                file_list = os.listdir(folder)
-            except:
-                file_list = []
-
-            fnames = [
-                f
-                for f in file_list
-                if os.path.isfile(os.path.join(folder, f))
-                and f.lower().endswith((".png", ".gif"))
-            ]
-            window["-FILE LIST-"].update(fnames)
-        elif event == "-FILE LIST-":  # A file was chosen from the listbox
-            try:
-                filename = os.path.join(
-                    values["-FOLDER-"], values["-FILE LIST-"][0]
-                )
-                # TODO:store image in memory
-                print(filename)
+    with mp_selfie_segmentation.SelfieSegmentation(
+     model_selection=1) as selfie_segmentation:
+        cap = cv2.VideoCapture(0)
+        while True:
+            event, values = window.read(timeout=20)
+            if event == "Exit" or event == sg.WIN_CLOSED:
+                break
             
-            #window["-IMAGE-"].update(filename=filename)
+            elif event == "-FOLDER-":
+                folder = values["-FOLDER-"]
+                try:
+                    # Get list of files in folder
+                    file_list = os.listdir(folder)
+                except:
+                    file_list = []
 
-            except:
-                pass
+                fnames = [
+                    f
+                    for f in file_list
+                    if os.path.isfile(os.path.join(folder, f))
+                    and f.lower().endswith((".png", ".gif"))
+                ]
+                window["-FILE LIST-"].update(fnames)
+            elif event == "-FILE LIST-":  # A file was chosen from the listbox
+                try:
+                    filename = os.path.join(
+                        values["-FOLDER-"], values["-FILE LIST-"][0]
+                    )
+                    # TODO:store image in memory
+                    print(filename)
+                
+                #window["-IMAGE-"].update(filename=filename)
 
-        ret, frame = cap.read()
+                except:
+                    pass
 
-        if values["-THRESH-"]:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)[:, :, 0]
-            frame = cv2.threshold(
-                frame, values["-THRESH SLIDER-"], 255, cv2.THRESH_BINARY
-            )[1]
-        elif values["-BLUR-"]:
-            frame = cv2.GaussianBlur(frame, (21, 21), values["-BLUR SLIDER-"])
-        elif values["-HUE-"]:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            frame[:, :, 0] += int(values["-HUE SLIDER-"])
-            frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+            ret, frame = cap.read()
+
+            if values["-THRESH-"]:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)[:, :, 0]
+                frame = cv2.threshold(
+                    frame, values["-THRESH SLIDER-"], 255, cv2.THRESH_BINARY
+                )[1]
+            elif values["-BLUR-"]:
+                frame = cv2.GaussianBlur(frame, (21, 21), values["-BLUR SLIDER-"])
+            elif values["-HUE-"]:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                frame[:, :, 0] += int(values["-HUE SLIDER-"])
+                frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
 
 
-        imgbytes = cv2.imencode(".png", frame)[1].tobytes()
-        window["-IMAGE-"].update(data=imgbytes)
+            imgbytes = cv2.imencode(".png", frame)[1].tobytes()
+            window["-IMAGE-"].update(data=imgbytes)
+            
+            bg_image = None
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
 
-    window.close()
+            # Flip the image horizontally for a later selfie-view display, and convert
+            # the BGR image to RGB.
+            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            results = selfie_segmentation.process(image)
+
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            # Draw selfie segmentation on the background image.
+            # To improve segmentation around boundaries, consider applying a joint
+            # bilateral filter to "results.segmentation_mask" with "image".
+            condition = np.stack(
+            (results.segmentation_mask,) * 3, axis=-1) > 0.1
+            # The background can be customized.
+            #   a) Load an image (with the same width and height of the input image) to
+            #      be the background, e.g., bg_image = cv2.imread('/path/to/image/file')
+            #   b) Blur the input image by applying image filtering, e.g.,
+            #      bg_image = cv2.GaussianBlur(image,(55,55),0)
+            if bg_image is None:
+                bg_image = np.zeros(image.shape, dtype=np.uint8)
+                bg_image[:] = BG_COLOR
+                output_image = np.where(condition, image, bg_image)
+
+                cv2.imshow('MediaPipe Selfie Segmentation', output_image)
+
+        window.close()
+        cap.release()
 
 main()
